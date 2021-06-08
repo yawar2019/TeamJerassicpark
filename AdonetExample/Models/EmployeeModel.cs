678b68497404c3058e12f40a0e938ff0e62cca54
yawar2019 <yawarali17@gmail.com>
2021-06-08 22:08:09 +0530

    commit

---
+++ AdonetExample/Models/EmployeeModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AdonetExample.Models
+{
+    public class EmployeeModel
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int EmpSalary { get; set; }
+    }
+
+    public class EmployeeContext
+    {
+        SqlConnection con = new SqlConnection("Data Source=AZAM-PC\\SQLEXPRESS;Initial Catalog=Employee;Integrated Security=true");//(User Id = sa; Password=123) to connect to database
+        public List<EmployeeModel> GetAllEmployee()
+        {
+
+
+        }
+
+    }
+}
